562cd70b7a87b821ae9b7fa8ed88d4cd2e714e1b
tejaswini-a07 <tallagadda@altir.co>
2025-05-02 12:34:13 +0530

    add files:

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ScoreTracker from "./ScoreTracker";
+
+function App() {
+  return (
+    <div>
+      <ScoreTracker />
+    </div>
+  );
+}
+
+export default App;
